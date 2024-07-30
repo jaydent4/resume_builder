d6c7f9f1347d9cd994dc938468a2a21947ff6bcf
Jayden Truong <jaydentbid@gmail.com>
2024-07-30 15:02:58 -0700

    Edits to text_file.py

--- text_editor.py
+++ text_editor.py
@@ -1,3 +1,34 @@
+# Check the version of Python
 import sys
 v = sys.version
-print(v)
+if int(v[0]) >= 3:
+    from tkinter import ttk
+    from tkinter import filedialog as fd
+else:
+    raise Exception(f"Python 3.X or higher is required. You currently have Python {v}")
+
+# Create a new file; change name and delete current text
+def new_file():
+    global filename
+    filename = "Untitled"
+    text.delete(0.0, ttk.END)
+
+def save():
+    global filename
+    t = text.get(0.0, END)
+    file = open(filename, 'w')
+    file.write(t)
+    file.close()
+
+def save_as():
+    file = fd.asksaveasfile(mode = 'w', defaultextension = '.txt')
+    t = text.get(0.0, ttk.END)
+    t = t.rstrip()
+    try:
+        file.write(t)
+    except: # should never reach this point
+        fd.showerror(Title="ERROR", message ="Unable to save file")
+
+def openFile():
+    f = fd.askopenfile(mode='r')
+
