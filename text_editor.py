a7a21ac63bf38848b9205fb05c51e5247284260c
Jayden Truong <jaydent@gmail.com>
2024-09-25 20:18:04 -0700

    testing

--- text_editor.py
+++ text_editor.py
@@ -52,35 +52,35 @@ def update_scrollable_region(root, canvas):
     canvas.configure(scrollregion=canvas.bbox("all"))
 
 
-# Create a new file; change name and delete current text
-def new_file(text):
-    global filename
-    filename = "Untitled"
-    text.delete(0.0, tk.END)
+# # Create a new file; change name and delete current text
+# def new_file(text):
+#     global filename
+#     filename = "Untitled"
+#     text.delete(0.0, tk.END)
 
-# Save current file 
-def save(text):
-    global filename
-    t = text.get(0.0, tk.END)
-    file = open(filename, 'w')
-    file.write(t)
-    file.close()
+# # Save current file 
+# def save(text):
+#     global filename
+#     t = text.get(0.0, tk.END)
+#     file = open(filename, 'w')
+#     file.write(t)
+#     file.close()
 
-def save_as(text):
-    # prompt to save each of the entries within a text file
-    file = fd.asksaveasfile(mode = 'w', defaultextension = '.txt')
-    t = text.get(0.0, tk.END)
-    t = t.rstrip()
-    try:
-        file.write(t)
-    except: # should never reach this point
-        tk.showerror(Title="ERROR", message ="Unable to save file")
+# def save_as(text):
+#     # prompt to save each of the entries within a text file
+#     file = fd.asksaveasfile(mode = 'w', defaultextension = '.txt')
+#     t = text.get(0.0, tk.END)
+#     t = t.rstrip()
+#     try:
+#         file.write(t)
+#     except: # should never reach this point
+#         tk.showerror(Title="ERROR", message ="Unable to save file")
 
-def open_file(text):
-    f = fd.askopenfile(mode='r')
-    t = f.read()
-    text.delete(0.0, tk.END)
-    text.insert(0.0, t)
+# def open_file(text):
+#     f = fd.askopenfile(mode='r')
+#     t = f.read()
+#     text.delete(0.0, tk.END)
+#     text.insert(0.0, t)
 
 if __name__ == "__main__":
     main()
